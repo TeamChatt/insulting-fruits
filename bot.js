2501d7889a718bc2ad8eea752eff7d5241c4657a
Matt Kaemmerer <matthew.kaemmerer@gmail.com>
2019-05-24 21:30:57 +0900

    Use .env to manage environment variables

--- bot.js
+++ bot.js
@@ -1,6 +1,13 @@
 const Twit = require('twit')
-const config = require('./config')
 const generate = require('./generate')
+require('dotenv').config()
+
+const config = {
+  consumer_key:         process.env.CONSUMER_KEY,
+  consumer_secret:      process.env.CONSUMER_SECRET,
+  access_token:         process.env.ACCESS_TOKEN,
+  access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
+}
 
 const T = new Twit(config)
 
